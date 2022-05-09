a552f852efc1131d9756fb1c2a9a0a57c62be8b6
flvSantos15 <flvSantos300@gmail.com>
2022-05-08 21:58:46 -0300

    just testing

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const jsonServer =  require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('server.json')
 const middlewares = jsonServer.defaults()
-const port = process.env.PORT || 3333
+const port = process.env.PORT || 3000
 
 server.use(middlewares)
 server.use(router)
